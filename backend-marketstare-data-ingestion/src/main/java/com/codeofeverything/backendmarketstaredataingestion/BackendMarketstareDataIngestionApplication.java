0c02a4f705a25b172bb36c704f3bbdb0a1b6d770
Avenash Kumar <avenash_kumar@hotmail.com>
2021-07-04 00:59:41 -0400

    Add spring boot project for backend-marketstare-data-ingestion

---
+++ backend-marketstare-data-ingestion/src/main/java/com/codeofeverything/backendmarketstaredataingestion/BackendMarketstareDataIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.codeofeverything.backendmarketstaredataingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMarketstareDataIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMarketstareDataIngestionApplication.class, args);
+	}
+
+}
