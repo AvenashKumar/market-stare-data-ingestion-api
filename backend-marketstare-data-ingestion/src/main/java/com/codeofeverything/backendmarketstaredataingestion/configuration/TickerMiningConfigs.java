e0802aaf5a19b30aead84fe9bdbe31b79a78c1c8
Avenash Kumar <avenash_kumar@hotmail.com>
2021-07-05 19:21:47 -0400

    Add mining rule - TICKER_MAX_LENGTH

--- backend-marketstare-data-ingestion/src/main/java/com/codeofeverything/backendmarketstaredataingestion/configuration/TickerMiningConfigs.java
+++ backend-marketstare-data-ingestion/src/main/java/com/codeofeverything/backendmarketstaredataingestion/configuration/TickerMiningConfigs.java
@@ -2,6 +2,7 @@ package com.codeofeverything.backendmarketstaredataingestion.configuration;
 
 import com.codeofeverything.backendmarketstaredataingestion.service.rule.mining.ETickerMiningRule;
 import com.codeofeverything.backendmarketstaredataingestion.service.rule.mining.ITickerMiningRule;
+import com.codeofeverything.backendmarketstaredataingestion.service.rule.mining.TickerMaxLengthRule;
 import com.codeofeverything.backendmarketstaredataingestion.service.rule.mining.TickerUpperCaseRule;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -10,19 +11,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MiningConfigs {
+public class TickerMiningConfigs {
   private final TickerUpperCaseRule tickerUpperCaseRule;
+  private final TickerMaxLengthRule tickerMaxLengthRule;
 
   @Autowired
-  public MiningConfigs(
-      TickerUpperCaseRule tickerUpperCaseRule) {
+  public TickerMiningConfigs(
+      TickerUpperCaseRule tickerUpperCaseRule,
+      TickerMaxLengthRule tickerMaxLengthRule) {
     this.tickerUpperCaseRule = tickerUpperCaseRule;
+    this.tickerMaxLengthRule = tickerMaxLengthRule;
   }
 
   @Bean
   Map<ETickerMiningRule, ITickerMiningRule> tickerMiningRulesMap(){
     Map<ETickerMiningRule, ITickerMiningRule> mapTickerMiningRules = new LinkedHashMap<>();
     mapTickerMiningRules.put(ETickerMiningRule.TICKER_UPPER_CASE, tickerUpperCaseRule);
+    mapTickerMiningRules.put(ETickerMiningRule.TICKER_MAX_LENGTH, tickerMaxLengthRule);
+
     return mapTickerMiningRules;
   }
 }
---
+++ backend-marketstare-data-ingestion/src/main/java/com/codeofeverything/backendmarketstaredataingestion/service/rule/mining/TickerMaxLengthRule.java
@@ -0,0 +1,21 @@
+package com.codeofeverything.backendmarketstaredataingestion.service.rule.mining;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TickerMaxLengthRule implements ITickerMiningRule{
+
+  @Override
+  public boolean validate(String text) {
+    if (ObjectUtils.isEmpty(text))
+      return false;
+
+    return text.length() <= 5;
+  }
+
+  @Override
+  public ETickerMiningRule getMiningRuleName() {
+    return ETickerMiningRule.TICKER_MAX_LENGTH;
+  }
+}
